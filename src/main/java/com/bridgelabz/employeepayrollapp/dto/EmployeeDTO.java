d6eb81c2e97dcd82e31dd548538ff533512351b9
Miracle0312 <prajapatidhani06@gmail.com>
2025-02-27 16:25:20 +0530

    [Dhani] Add. Use Lombok Library to auto generate getter setter

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,28 +1,21 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
 import com.bridgelabz.employeepayrollapp.entity.EmployeeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class EmployeeDTO {
     private String name;
     private double salary;
-    
-    public EmployeeDTO() {}
 
     public EmployeeDTO(EmployeeEntity entity) {
         this.name = entity.getName();
         this.salary = entity.getSalary();
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public double getSalary() {
-        return salary;
-    }
-    public void setSalary(double salary) {
-        this.salary = salary;
     }
 }
--- src/main/java/com/bridgelabz/employeepayrollapp/entity/EmployeeEntity.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/entity/EmployeeEntity.java
@@ -1,53 +1,22 @@
 package com.bridgelabz.employeepayrollapp.entity;
 
 import jakarta.persistence.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "employees")
 public class EmployeeEntity {
-    public static final Logger logger= LoggerFactory.getLogger(EmployeeEntity.class);
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String department;
     private double salary;
-
-    //default constructor
-    public EmployeeEntity() {}
-
-    //parameterized constructor
-    public EmployeeEntity(Long id, String name, String department, double salary) {
-        this.id = id;
-        this.name = name;
-        this.department = department;
-        this.salary = salary;
-    }
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getDepartment() {
-        return department;
-    }
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-    public double getSalary() {
-        return salary;
-    }
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
