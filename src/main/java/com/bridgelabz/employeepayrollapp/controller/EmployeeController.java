6ff658991210f2d2a9279f9b436db79aab1de8cf
Miracle0312 <prajapatidhani06@gmail.com>
2025-02-27 16:42:42 +0530

    [Dhani] Add. Use Lombok Library to auto generate logging by @Slf4j

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.bridgelabz.employeepayrollapp.controller;
 import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
 import com.bridgelabz.employeepayrollapp.entity.EmployeeEntity;
 import com.bridgelabz.employeepayrollapp.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,23 +13,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-//UC_3- Ability for the Services Layer to store the Employee Payroll Data
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
-    public static final Logger logger= LoggerFactory.getLogger(EmployeeController.class);
 
     @Autowired
     public EmployeeService employeeService;
     @PostMapping
     public ResponseEntity<?> addEmployees(@RequestBody EmployeeDTO employee){
-        logger.info("Adding a new employee: {}", employee.getName());
+        log.info("Adding a new employee: {}", employee.getName());
         EmployeeEntity saveEmployee= employeeService.saveEmployees(employee);
         return ResponseEntity.ok(new EmployeeDTO(saveEmployee));
     }
     @GetMapping
     public ResponseEntity<List<EmployeeEntity>> getAllEmployees(){
-        logger.info("Get all employee details");
+        log.info("Get all employee details");
         List<EmployeeEntity> getEmployees=  employeeService.getAllEmployees();
         List<EmployeeDTO> getEmployeeDetails= new ArrayList<>();
         for(EmployeeEntity entity: getEmployees){
@@ -38,7 +38,7 @@ public class EmployeeController {
     }
     @GetMapping("/{id}")
     public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable Long id){
-        logger.info("Get employee details by id: {}", id);
+        log.info("Get employee details by id: {}", id);
         Optional<EmployeeEntity> getEmployee = employeeService.getEmployeeById(id);
         if(getEmployee.isPresent()){
             return ResponseEntity.ok(new EmployeeDTO(getEmployee.get()));
@@ -47,13 +47,13 @@ public class EmployeeController {
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<EmployeeDTO> deleteEmployee(@PathVariable Long id){
-        logger.info("Delete employee.");
+        log.info("Delete employee.");
         employeeService.deleteEmployee(id);
         return ResponseEntity.noContent().build();
     }
     @PutMapping
     public ResponseEntity<EmployeeDTO> updateEmployee(@PathVariable Long id, @RequestBody EmployeeDTO employeeDTO){
-        logger.info("Updated employee details.");
+        log.info("Updated employee details.");
         EmployeeEntity updateEmployee= employeeService.updateEmployee(id, employeeDTO);
         return ResponseEntity.ok(new EmployeeDTO(updateEmployee));
     }
